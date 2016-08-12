d143eef345b497663eb355b68c2339e88dfd9380
Artem V. Navrotskiy <bozaro@yandex.ru>
2016-08-12 14:00:24 +0300

    Fix @NotNull/@Nullable reference

--- src/test/java/ru/bozaro/p4/Example.java
+++ src/test/java/ru/bozaro/p4/Example.java
@@ -1,7 +1,7 @@
 package ru.bozaro.p4;
 
-import com.sun.istack.internal.NotNull;
-import com.sun.istack.internal.Nullable;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import ru.bozaro.p4.proto.Client;
 import ru.bozaro.p4.proto.Message;
 
